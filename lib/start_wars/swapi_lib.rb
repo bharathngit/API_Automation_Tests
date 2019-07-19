dac49b8c8d419109c5d89c0e4ce872d84dfc133e
Bharath <bharath.nadukatla@gmail.com>
2019-07-19 15:19:31 -0700

    Added logger and logs/debug.log file

--- lib/start_wars/swapi_lib.rb
+++ lib/start_wars/swapi_lib.rb
@@ -1,10 +1,16 @@
 require "swapi" # Ruby helper llibrary for Star wars api
 require "json"
+require 'logger'
+
+require_relative "logger.rb"
+
 
 module StarWarsMethods
 
   def verify_character(film_name, char_name)
-    puts "verify_character #{char_name} and #{film_name}."
+    $logger.info(__method__) {"started."}
+    $logger.info(__method__) {"verify_character #{char_name} and #{film_name}."}
+
     # Get films resource using swapi helper library method 'get_all'
     films_resource = Swapi.get_all "films"
     # Parse the resource data into Ruby Hash using JSON
@@ -12,22 +18,22 @@ module StarWarsMethods
     films_hash = films_json_hash["results"]
     found = false
     for film in films_hash
-      puts "Film: #{film["title"]}"
+      $logger.info(__method__) {"Film: #{film["title"]}"}
 
       # For each film in the hash look for the required film_name
       if film["title"]== film_name
-        puts "Film found!"
-        puts film["title"]
+        $logger.info(__method__) {"Film found!"}
+        $logger.info(__method__) {film["title"]}
         # for each character in the film look for char_name
-        puts "Charaters in this film:"
+        $logger.info(__method__) {"Charaters in this film:"}
         for char in film["characters"]
           # User swapi's method 'get_person' to get resource and parse it into a ruby hash
           chars_json_hash = JSON.parse Swapi.get_person(char.gsub!(/\D/, ""))
-          puts (chars_json_hash)["name"]
+          $logger.info(__method__) {(chars_json_hash)["name"]}
           # Search for the character name equal to char_name
           if (chars_json_hash)["name"]== char_name
-            puts "Character found!"
-            puts "#{char_name} was in the film!"
+            $logger.info(__method__) {"Character found!"}
+            $logger.info(__method__) {"#{char_name} was in the film!"}
             found = true
             break
           end
@@ -36,32 +42,33 @@ module StarWarsMethods
       end
     end
     if found
+      $logger.info(__method__) {"ended."}
       return true
     else
-      puts "Character '#{char_name}' not found in the film '#{film_name}'"
+      $logger.error(__method__) {"Character '#{char_name}' not found in the film '#{film_name}'"}
       return found
     end
   rescue StandardError => e
-    puts e.message
+    $logger.error(__method__) {e.message}
     false
   end
 
   def verify_starship(ship_name)
-    puts "verify_starship #{ship_name} ."
+    $logger.info(__method__) {"verify_starship #{ship_name} ."}
     # Get starship resource using swapi helper library method 'get_all'
     ship_resource = Swapi.get_starship "?search=#{ship_name}"
     # Parse the resource data into Ruby Hash using JSON
     ship_json_hash = JSON.parse ship_resource
-    puts ship_json_hash
+    $logger.info(__method__) {ship_json_hash}
     ship_hash = ship_json_hash["results"]
     if ship_hash.empty?
       raise "No data found for #{ship_name}"
     else
-      puts "#{ship_hash[0]["name"]} is a Starship!"
+      $logger.info(__method__) {"#{ship_hash[0]["name"]} is a Starship!"}
       true
     end
     rescue StandardError => e
-    puts e.message
+    $logger.error(__method__) {e.message}
     false
   end #verify_starship
 
@@ -72,25 +79,25 @@ module StarWarsMethods
     people_json_hash = JSON.parse people_resource
 
     people_hash = people_json_hash["results"]
-    puts people_hash
+    $logger.info(__method__) {people_hash}
     species_uri = people_hash[0]['species']
-    puts species_uri
+    $logger.info(__method__) {species_uri}
     if people_hash.empty?
       raise "No data found for #{person_name}"
     else
     # Verify the species
       species_json_hash = JSON.parse Swapi.get_species(species_uri[0].gsub!(/\D/, ""))
-      puts species_json_hash
+      $logger.info(__method__) {species_json_hash}
       species_name = species_json_hash["name"]
       if species_name == species_type
-        puts "#{person_name} belongs to #{species_type}!"
+        $logger.info(__method__) {"#{person_name} belongs to #{species_type}!"}
         return true
       else
         raise "Error!#{person_name} Doesnt belongs to #{species_type}!"
       end
     end
     rescue StandardError => e
-    puts e.message
+    $logger.error(__method__) {e.message}
     false
   end #verify_species
 
@@ -104,17 +111,17 @@ module StarWarsMethods
     for ship_hash in starship_array
       for exp_key in keys_array
         if ship_hash.key?(exp_key)
-          puts "#{exp_key} field exists!"
+          $logger.info(__method__) {"#{exp_key} field exists!"}
           flag = true
         else
-          puts "#{key} field doesnt exists!"
+          $logger.error(__method__) {"#{key} field doesnt exists!"}
           flag = false
         end
       end
     end
     flag
     rescue StandardError => e
-    puts e.message
+    $logger.error(__method__) {e.message}
     flag
   end #starships_response_contains
 
@@ -125,7 +132,7 @@ module StarWarsMethods
     starship_json_hash = JSON.parse ships_resource
     # Get the initial ships count
     ship_count = starship_json_hash["count"]
-    puts "Ship count required: #{ship_count}"
+    $logger.info(__method__) {"Ship count required: #{ship_count}"}
     # Set the flag and counters for ships and pages
     has_next = true
     counter = 0
@@ -133,18 +140,18 @@ module StarWarsMethods
 
     # Loop until there is no next page in "next" key of the hash.
     while(has_next)
-      puts "Current page:#{page_count}********************"
+      $logger.info(__method__) {"Current page:#{page_count}********************"}
       # count the number of ships in the current page
       for ship_hash in starship_json_hash["results"]
-        puts ship_hash["name"]
+        $logger.info(__method__) {ship_hash["name"]}
         counter = counter + 1
       end
       # Ship count at the end of current page
-      puts "No. of space ships:#{counter}"
+      $logger.info(__method__) {"No. of space ships:#{counter}"}
 
       # Check whether there is next page
       if starship_json_hash["next"]!=nil
-        puts "Next page exists*******************"
+        $logger.info(__method__) {"Next page exists*******************"}
         has_next = true
         page_count = page_count + 1
         # Get the resource and assign it to starship_json_hash
@@ -152,20 +159,20 @@ module StarWarsMethods
         starship_json_hash = JSON.parse next_page_ship_resource
       else
         # Break out of the loop if there are no more pages left
-        puts "End of pagination.******************"
+        $logger.info(__method__) {"End of pagination.******************"}
         break
       end
     end
     # Compare final count value of 'counter' with initial count value 'ship_count'
     if(counter == ship_count)
-      puts "**********Total ship counts matched!***********"
+      $logger.info(__method__) {"**********Total ship counts matched!***********"}
       return true
     else
-      puts "**********Total ship counts DID NOT match!***********"
+      $logger.error(__method__) {"**********Total ship counts DID NOT match!***********"}
       return false
     end
     rescue StandardError => e
-    puts e.message
+    $logger.error(__method__) {e.message}
     false
   end # verify_starship_count
 end # module
--- spec/start_wars_spec.rb
+++ spec/start_wars_spec.rb
@@ -1,35 +1,37 @@
-require_relative "swapi_lib"
+require 'logger'
+require_relative "../lib/start_wars/swapi_lib"
+require_relative "../lib/start_wars/logger"
 include StarWarsMethods
 RSpec.describe StartWars do
 
   it "Assert that Obi-Wan Kenobi was in the film A New Hope" do
-    puts "Test 1. Assert that Obi-Wan Kenobi was in the film A New Hope"
+    $logger.info {"Test 1. Assert that Obi-Wan Kenobi was in the film A New Hope"}
     expect(true).to eq(verify_character("A New Hope", "Obi-Wan Kenobi"))
   end
 
   it "Assert that the Enterprise is a starship (yes, this should fail)" do
-    puts "Test 2. Assert that the Enterprise is a starship (yes, this should fail)"
+    $logger.info {"Test 2. Assert that the Enterprise is a starship (yes, this should fail)"}
     expect(verify_starship("Enterprise")).to eq(false)
   end
 
   it "Assert that Chewbacca is a Wookiee" do
-    puts "Test 3. Assert that Chewbacca is a Wookiee"
+    $logger.info {"Test 3. Assert that Chewbacca is a Wookiee"}
     expect(verify_species("Chewbacca","Wookiee")).to eq(true)
   end
 
   it "Assert that the /starships endpoint returns the fields below:
-    ○ name
-    ○ model
-    ○ crew
-    ○ hyperdrive_rating
-    ○ pilots
-    ○ films" do
-    puts "Test 4. Assert that the /starships endpoint returns the fields"
+      ○ name
+      ○ model
+      ○ crew
+      ○ hyperdrive_rating
+      ○ pilots
+      ○ films" do
+    $logger.info {"Test 4. Assert that the /starships endpoint returns the fields"}
     expect(starships_response_contains(["name","model","crew","hyperdrive_rating","pilots","films"])).to eq(true)
   end
 
   it "Assert that the /starships count returned is correct by paging through the results" do
-    puts "Test 5.Assert that the /starships count returned is correct by paging through the results"
+    $logger.info {"Test 5.Assert that the /starships count returned is correct by paging through the results"}
     expect(verify_starship_count).to eq(true)
   end
 end
